feea497cdff223f460753d2a311704b083c74640
Luis Martinez <lmartin1@stu.southtexascollege.edu>
2024-09-20 13:26:55 -0500

    finished Income Expenses component

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <script setup>
   import Header from './components/Header.vue';
   import Balance from './components/Balance.vue';
+  import IncomeExpenses from './components/IncomeExpenses.vue';
   import { ref, computed } from 'vue';
+  
   const transactions = ref([
     {id: 1, text:'Paycheck', amount: 700.00},
     {id: 2, text:'Water Bill', amount: -72.83},
@@ -15,6 +17,22 @@
     },0)
   })
 
+  const moneyIn = computed(()=>{
+    return transactions.value
+    .filter((x)=>x.amount>0)
+    .reduce((acc, x)=>{
+      return acc+x.amount
+    },0)
+  })
+
+  const moneyOut = computed(()=>{
+    return transactions.value
+    .filter((x)=>x.amount<0)
+    .reduce((acc, x)=>{
+      return acc+x.amount
+    },0)
+  })
+
 </script>
 
 
@@ -22,6 +40,7 @@
   <Header></Header>
   <div class="container">
     <Balance :total="sum"></Balance>
+    <IncomeExpenses :income="moneyIn" :expense="moneyOut"></IncomeExpenses>
   </div>
 
 
---
+++ src/components/IncomeExpenses.vue
@@ -0,0 +1,31 @@
+<script setup>
+    import { defineProps } from 'vue';
+
+    const props = defineProps({
+        income: {
+            type: Number,
+            require: true,
+        },
+        expense: {
+            type: Number,
+            require: true,
+        }
+    })
+
+</script>
+
+<template>
+    <div class="inc-exp-container">
+        <dvi>
+            <h4>Income</h4>
+            <p id="money-plus" class="money plus">+${{ income }}</p>
+        </dvi>
+        <dvi>
+            <h4>Expense</h4>
+            <p id="money-minus" class="money minus">-${{ expense }}</p>
+        </dvi>
+    </div>
+
+
+
+</template>
